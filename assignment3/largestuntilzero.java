9336e9104d85beff9877d075b6ede2e3ec459eca
ayush <ayushkumar44903@gmail.com>
2025-07-12 15:36:59 +0530

    largest number until zero

---
+++ assignment3/largestuntilzero.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class largestuntilzero {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number;
+        int largest=0;
+
+        while(true){
+            number = input.nextInt();
+
+            if (number==0){
+                break;
+            }
+            if(number>=largest){
+                largest= number;
+            }
+        }
+        System.out.println("The largest no is: "+largest);
+    }
+    
+}
