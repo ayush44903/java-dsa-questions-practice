2a3fbf3475744ff4cf72b107a3e9e34ec6186145
ayush <ayushkumar44903@gmail.com>
2025-07-14 19:51:17 +0530

    largest and smallest number function

--- functions/largestsmallestusingfunction.java
+++ functions/largestsmallestusingfunction.java
@@ -1,7 +1,7 @@
 package functions;
 import java.util.Scanner;
 
-public class largestusingfunction {
+public class largestsmallestusingfunction {
     public static int largest(int first, int second, int third){
         int max =  first;
         if(first <second){
@@ -13,13 +13,26 @@ public class largestusingfunction {
         return max;
     }
 
+    public static int smallest (int first, int second , int third ){
+        int min = first;
+        if(first>second){
+            min = second;
+        }
+        if(second>third){
+            min = third;
+        }
+        return min;
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int first = input.nextInt();
         int second = input.nextInt();
         int third = input.nextInt();
         int max = largest(first, second, third);
+        int min = smallest(first, second, third);
         System.out.println("The largest number among these is: "+max);
+        System.out.println("The minimum number among these is: "+min);
         input.close();
     }
 }
